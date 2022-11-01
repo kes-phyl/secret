1dbc0d0dd60254568d9ddf3d7443f7741cbd9395
kes-phyl <danphylbert@gmail.com>
2022-11-01 23:57:16 +0000

    setting up logout route

--- app.js
+++ app.js
@@ -100,8 +100,14 @@ app.get('/secrets', function(req,res){
     }
 })
 app.get('/logout', function(req,res){
-    req.logout();
-   res.redirect('/')
+    req.logout(function(err){
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect('/') 
+        }
+    });
+   
 })
 
 app.post('/register', function(req, res){

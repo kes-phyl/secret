90e0219b7ebc4176ce7e629b0c199a3f57419ffb
kes-phyl <danphylbert@gmail.com>
2022-11-11 23:21:10 +0000

    add db connection url to env

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(passport.session())
 
 
 // connecting to the database and creating a model.. 
-mongoose.connect('mongodb://localhost:27017/userDB');
+mongoose.connect(process.env.URL);
 
 
 
